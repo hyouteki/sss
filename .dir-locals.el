e14d827fbd0767dd55a7d556b1b27dfb9ee86a2f
hyouteki <lakshay21060@iiitd.ac.in>
2025-01-10 23:03:16 +0530

    add turn handling, new emacs local config file

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+((html-mode
+  (indent-tabs-mode . nil)
+  (tab-width . 4))
+ (css-mode
+  (indent-tabs-mode . nil)
+  (tab-width . 4))
+ (js-mode
+  (indent-tabs-mode . nil)
+  (tab-width . 4)))
--- main.js
+++ main.js
@@ -1,7 +1,40 @@
+var context = {};
+context.currentTurn = "white";
+
 var config = {
 	draggable: true,
 	dropOffBoard: "snapback",
 	position: "start"
 }
 
-var board = ChessBoard("board", config)
+function updateTurnIndicator(currentTurn) {
+    const turnElement = document.getElementById("currentTurn");
+    if (currentTurn === "white") {
+        turnElement.innerHTML = "&nbsp;WHITE&nbsp;";
+        turnElement.style.color = "black";
+        turnElement.style.backgroundColor = "white";
+        turnElement.style.fontStyle = "italic";
+    } else {
+        turnElement.innerHTML = "&nbsp;BLACK&nbsp;";
+        turnElement.style.color = "white";
+        turnElement.style.backgroundColor = "black";
+        turnElement.style.fontStyle = "italic";
+    }
+}
+
+// Entry point
+var board = ChessBoard("board", {
+    draggable: true,
+    onDragStart: (source, piece, position, orientation) => {
+        return ((!(context.currentTurn === "white" && piece.startsWith("b"))) &&
+                (!(context.currentTurn === "black" && piece.startsWith("w"))));
+    },
+    onDrop: (source, target, piece) => {
+        if (source === target) return "snapback";
+        context.currentTurn = (context.currentTurn === "white")? "black": "white";
+        updateTurnIndicator(context.currentTurn);
+    },
+    position: "start"
+});
+
+updateTurnIndicator(context.currentTurn);
